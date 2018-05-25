f351d18c2d9081d57319e3beb79852c6b05f3e3d
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-05-25 08:52:42 +0200

    Second lecture
    [FA-25]

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log('Hello World!');
+// Lecture: variables
+
+// var name = 'John';
+// console.log(name);
+
+// var lastName = 'Smith';
+// console.log(lastName);
+
+// var age = 26;
+// console.log(age);
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// Lecture: variables 2
+var name = 'John';
+var age = 26;
+
+var job, isMarried;
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age +
+' years old ' + job + '. Is he married? ' + isMarried +
+'.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age +
+' years old ' + job + '. Is he married? ' + isMarried +
+'.');
+
+var lastName = prompt('What is the last name');
+console.log(lastName);
+
+alert(name + ' is a ' + age +
+' years old ' + job + '. Is he married? ' + isMarried +
+'.');
