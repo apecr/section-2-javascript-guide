c7261adcccc29335bd681c333b162b60c69b3114
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-05-28 08:44:46 +0200

    Code challenge 2
    [FA-25]

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
-// Lecture: Loops
+// Coding challenge 2
 
-for (var i = 0; i < 10; i++) {
-  console.log(i);
-}
 
-var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+const years = [1990, 1987, 1986, 1996, 2001, 1956, 1960];
+const yearsFilled = years.map(element => element);
 
-for (var i = 0; i < names.length; i++) {
-  console.log(names[i]);
-}
+console.log(yearsFilled);
 
-names.reverse().forEach(element => console.log(element));
-
-console.log(names.reverse());
-
-// while loops
-var i = 0;
-while (i < names.length) {
-  console.log(names[i]);
-  i++;
-}
-
-// break and continue sentence
-
-for (var i = 1; i <= 5; i++) {
-  console.log(i);
-  if (i === 3) {
-    break;
+yearsFilled.forEach(yearOfBirht => {
+  if (2018 - yearOfBirht >= 18) {
+    return console.log('Full Age', yearOfBirht);
   }
-}
+  console.log('Not full age', yearOfBirht);
+});
 
-for (var i = 1; i <= 5; i++) {
-  if (i === 3) {
-    continue;
-  }
-  console.log(i);
-}
+const printFullAge = yearsOfBirht => {
+  const yearsOfBirthFilled = yearsOfBirht.map(element => element);
+  yearsOfBirthFilled.forEach(yearOfBirht => {
+    if (2018 - yearOfBirht >= 18) {
+      return console.log('Full Age', yearOfBirht);
+    }
+    console.log('Not full age', yearOfBirht);
+  });
+  return yearsOfBirthFilled.map(element => 2018 - element >= 18);
+};
+
+console.log(printFullAge([1965, 2008, 1992]));
